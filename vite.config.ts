ce16e3b4b1ddd56822e43d2f39c23c9547b964cb
Florian Heuberger <fh@flogersoft.de>
2026-03-07 22:51:22 +0100

    chore: add oxc

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import dts from 'vite-plugin-dts'
-import { resolve } from 'node:path'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'node:path';
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    dts({ tsconfigPath: './tsconfig.json' }),
-  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
@@ -23,4 +19,5 @@ export default defineConfig({
       },
     },
   },
-})
+  plugins: [vue(), dts({ tsconfigPath: './tsconfig.json' })],
+});
